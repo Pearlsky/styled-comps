8ed79c3ce51cce591efbc64c6c73c0513add4c0d
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-02 06:30:47 +0100

    style: write styled component for section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import styled from "styled-components";
+import WeekdayTitle from "./components/WeekdayTitle";
 
 const Title = styled.h1`
   color: orange;
+`;
+
+const Section = styled.div`
+  background-color: #ffffff;
+  border: solid 3px grey;
+  margin: 4px;
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: left;
+  font-size: 40px;
+  color: #ff6961;
 `;
 
 const App = () => {
   return (
     <div>
       <Title>Progress Tracker</Title>
+      <Section>
+        <WeekdayTitle day={"M"}></WeekdayTitle>
+      </Section>
+      <Section>
+        <WeekdayTitle day={"T"}></WeekdayTitle>
+      </Section><Section>
+        <WeekdayTitle day={"W"}></WeekdayTitle>
+      </Section><Section>
+        <WeekdayTitle day={"T"}></WeekdayTitle>
+      </Section><Section>
+        <WeekdayTitle day={"F"}></WeekdayTitle>
+      </Section><Section>
+        <WeekdayTitle day={"S"}></WeekdayTitle>
+      </Section><Section>
+        <WeekdayTitle day={"S"}></WeekdayTitle>
+      </Section>
     </div>
   );
 };
