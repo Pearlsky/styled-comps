426fd73235f4e396fd934fa5e67d6bb78f0e2a09
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-03 22:09:42 +0100

    refactor: modify root component to include new section & weekday title components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import styled from "styled-components";
-import WeekdayTitle from "./components/WeekdayTitle";
+import Section, { WeekdayTitle } from "./components/Section";
 
 const Title = styled.h1`
   color: orange;
-`;
-
-const Section = styled.div`
-  background-color: #ffffff;
-  border: solid 3px grey;
-  margin: 4px;
-  width: 300px;
-  height: 50px;
-  border-radius: 10px;
-  display: flex;
-  justify-content: left;
-  font-size: 40px;
-  color: #ff6961;
 `;
 
 const App = () => {
@@ -23,20 +10,25 @@ const App = () => {
     <div>
       <Title>Progress Tracker</Title>
       <Section>
-        <WeekdayTitle day={"M"}></WeekdayTitle>
+        <WeekdayTitle day="M"></WeekdayTitle>
       </Section>
       <Section>
-        <WeekdayTitle day={"T"}></WeekdayTitle>
-      </Section><Section>
-        <WeekdayTitle day={"W"}></WeekdayTitle>
-      </Section><Section>
-        <WeekdayTitle day={"T"}></WeekdayTitle>
-      </Section><Section>
-        <WeekdayTitle day={"F"}></WeekdayTitle>
-      </Section><Section>
-        <WeekdayTitle day={"S"}></WeekdayTitle>
-      </Section><Section>
-        <WeekdayTitle day={"S"}></WeekdayTitle>
+        <WeekdayTitle day="T"></WeekdayTitle>
+      </Section>
+      <Section>
+        <WeekdayTitle day="W"></WeekdayTitle>
+      </Section>
+      <Section>
+        <WeekdayTitle day="T"></WeekdayTitle>
+      </Section>
+      <Section>
+        <WeekdayTitle day="F"></WeekdayTitle>
+      </Section>
+      <Section>
+        <WeekdayTitle day="S"></WeekdayTitle>
+      </Section>
+      <Section>
+        <WeekdayTitle day="S"></WeekdayTitle>
       </Section>
     </div>
   );
