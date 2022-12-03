53712ec4b38161474ed3654522cce6d803d90e80
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-03 22:07:33 +0100

    style: write styled component to use props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: white;
+  background-color: ${({primary}) => primary ? 'red' : 'yellow'};
   border: none;
   padding: 1em 0.2em;
   margin: 0.5em 0.2em;
 `;
 
-const Button = () => {
-  return <StyledButton> Click Me </StyledButton>;
+const Button = ( {primary, text} ) => {
+  return <StyledButton primary={primary}> {text} </StyledButton>;
 };
 
 // note: props passed with just their names have a value of true
